120265a40cccd2bb19b774a8d48dbc3666d3dda1
kay <mrkrasin1@gmail.com>
2022-07-01 16:30:34 +0300

    add header

--- src/js/app.js
+++ src/js/app.js
@@ -7,5 +7,51 @@ import Swiper, { Navigation, Pagination } from 'swiper'
 
 const swiper = new Swiper()
 
-console.log('hello test 2');
-console.log("hhhhhhhhhhhhh");
+
+const isMobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android() ||
+			isMobile.BlackBerry() ||
+			isMobile.iOS() ||
+			isMobile.Opera() ||
+			isMobile.Windows())
+	}
+};
+
+if (isMobile.any()) {
+	document.body.classList.add('_touch');
+
+
+}
+
+else { document.body.classList.add('_pc') }
+
+const menuIcon = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (menuIcon){menuIcon.addEventListener('click', function(event){
+	document.body.classList.toggle('_lock')
+	menuIcon.classList.toggle('_active')
+	menuBody.classList.toggle('_active')
+})
+	
+}
+
